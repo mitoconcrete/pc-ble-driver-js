8a45932a4a5852f5863dae5eb4e51712e19e77d2
Florian Pichler <florian.pichler@essentim.com>
2019-03-22 16:44:34 +0100

    added example test.js

---
+++ examples/test.js
@@ -0,0 +1,195 @@
+/* Copyright (c) 2010 - 2017, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Use in source and binary forms, redistribution in binary form only, with
+ * or without modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 2. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 3. This software, with or without modification, must only be used with a Nordic
+ *    Semiconductor ASA integrated circuit.
+ *
+ * 4. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @example examples/heart_rate_collector
+ *
+ * @brief Heart Rate Collector Sample Application main file.
+ *
+ * This file contains the source code for a sample application that acts as a BLE Central device.
+ * This application scans for a Heart Rate Sensor device and reads it's heart rate data.
+ * https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.heart_rate.xml
+ */
+
+'use strict';
+
+const api = require('../index');
+const path = require('path');
+
+const adapterFactory = api.AdapterFactory.getInstance(undefined, { enablePolling: false });
+
+
+/**
+ * Function to start scanning (GAP Discovery procedure, Observer Procedure).
+ *
+ * @param {Adapter} adapter Adapter being used.
+ * @returns {Promise} Resolves on successfully initiating the scanning procedure.
+ *                    If an error occurs, rejects with the corresponding error.
+ */
+function startScan(adapter) {
+    return new Promise((resolve, reject) => {
+        console.log('Started scanning...');
+
+        const scanParameters = {
+            active: false,
+            interval: 1000,
+            window: 1000,
+            timeout: 0,
+        };
+
+        adapter.startScan(scanParameters, err => {
+            if (err) {
+                reject(new Error(`Error starting scanning: ${err}.`));
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+/**
+ * Handling events emitted by adapter.
+ *
+ * @param {Adapter} adapter Adapter in use.
+ * @returns {void}
+ */
+function addAdapterListener(adapter) {
+    /**
+     * Handling error and log message events from the adapter.
+     */
+    adapter.on('logMessage', (severity, message) => { if (severity > 3) console.log(`${message}.`); });
+    adapter.on('error', error => { console.log(`error: ${JSON.stringify(error, null, 1)}.`); });
+
+    /**
+     * Handling the Application's BLE Stack events.
+     */
+    adapter.on('deviceConnected', device => {
+        console.log(`Device ${device.address}/${device.addressType} connected.`);
+    });
+
+    adapter.on('deviceDisconnected', device => {
+        console.log(`Device ${device.address} disconnected.`);
+
+        startScan(adapter).then(() => {
+            console.log('Successfully initiated the scanning procedure.');
+        }).catch(error => {
+            console.log(error);
+        });
+    });
+
+    adapter.on('deviceDiscovered', device => {
+        if (device.address === 'D8:BB:19:38:45:7D') {
+            console.log(`Discovered Flo's scope: ${ JSON.stringify(device.adData, null , 2) }.`);
+        }
+    });
+
+    adapter.on('scanTimedOut', () => {
+        console.log('scanTimedOut: Scanning timed-out. Exiting.');
+        process.exit(1);
+    });
+
+    adapter.on('characteristicValueChanged', attribute => {
+        if (attribute.uuid === BLE_UUID_HEART_RATE_MEASUREMENT_CHAR) {
+            console.log(`Received heart rate measurement: ${attribute.value}.`);
+        }
+    });
+}
+
+/**
+ * Opens adapter for use with the default options.
+ *
+ * @param {Adapter} adapter Adapter to be opened.
+ * @returns {Promise} Resolves if the adapter is opened successfully.
+ *                    If an error occurs, rejects with the corresponding error.
+ */
+function openAdapter(adapter) {
+    return new Promise((resolve, reject) => {
+        const baudRate = process.platform === 'darwin' ? 115200 : 1000000;
+        console.log(`Opening adapter with ID: ${adapter.instanceId} and baud rate: ${baudRate}...`);
+
+        adapter.open({ baudRate, logLevel: 'error' }, err => {
+            if (err) {
+                reject(Error(`Error opening adapter: ${err}.`));
+            }
+
+            resolve();
+        });
+    });
+}
+
+function help() {
+    console.log(`Usage: ${path.basename(__filename)} <PORT> <SD_API_VERSION>`);
+    console.log();
+    console.log('PORT is the UART for the adapter. For example /dev/ttyS0 on Unix based systems or COM1 on Windows based systems.');
+    console.log('SD_API_VERSION can be v2 or v3. nRF51 series uses v2.');
+    console.log();
+    console.log('It is assumed that the nRF device has been programmed with the correct connectivity firmware.');
+}
+
+/**
+ * Application main entry.
+ */
+if (process.argv.length !== 4) {
+    help();
+    process.exit(-1);
+} else {
+    const [,, port, apiVersion] = process.argv;
+
+    if (port == null) {
+        console.error('PORT must be specified');
+        process.exit(-1);
+    }
+
+    if (apiVersion == null) {
+        console.error('SD_API_VERSION must be provided');
+        process.exit(-1);
+    } else if (!['v2', 'v3'].includes(apiVersion)) {
+        console.error(`SD_API_VERSION must be v2 or v3, argument provided is ${apiVersion}`);
+        process.exit(-1);
+    }
+
+    const adapter = adapterFactory.createAdapter(apiVersion, port, '');
+    addAdapterListener(adapter);
+
+    openAdapter(adapter).then(() => {
+        console.log('Opened adapter.');
+        return startScan(adapter);
+    }).then(() => {
+        console.log('Scanning.');
+    }).catch(error => {
+        console.log(error);
+        process.exit(-1);
+    });
+}
